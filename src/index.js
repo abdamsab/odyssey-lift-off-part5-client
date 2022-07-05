3b61e8663ca1bf6cad93757b2db7d680adbd50d8
Abdamsab <abdamsabinfo@gmail.com>
2022-07-05 21:23:49 +0100

    test with course server

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
 
-  uri: "https://mygraphqlapp-server.herokuapp.com/",    //change to YOUR own production server
-
+  uri: "https://server-catstronauts.herokuapp.com/",    //change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
-  
+
 });
 
 ReactDOM.render(
